27ae1a13aa98fa1f56721fed2fa3d4d46b9bb121
Valerii Bihun <valeriibihun.co@gmail.com>
2022-05-18 06:45:59 -0600

    adding a back button on the second page

--- pages/suggestion.js
+++ pages/suggestion.js
@@ -1,5 +1,6 @@
 import Suggestion from "../components/Suggestion";
 import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 export default function Suggest() {
   // Setting up states to keep all of the current data;
@@ -24,6 +25,9 @@ export default function Suggest() {
     setResponse(value);
   };
 
+  // // Setting up a navigate variable for a 'go-back' button;
+  // const navigate = useNavigate();
+
   return (
     <div className="h-screen flex flex-col items-center justify-center response-page ">
       <p className="text-center text-white bg-[#231437] rounded-lg p-4 shadow-xl shadow-black m-3 ">
@@ -43,6 +47,14 @@ export default function Suggest() {
             ))
           : null}
       </div>
+      <>
+        <button
+          className="absolute bottom-0  w-1/6 shadow-sm shadow-black p-2 mt-3 text-white bg-gradient-to-r from-purple-900 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-xl  text-center mr-2 mb-2 back-button"
+          onClick={() => (location.href = "/")}
+        >
+          Back
+        </button>
+      </>
     </div>
   );
 }
