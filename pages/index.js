3812999c4d38349563412b357a5ffc7f036d2cfa
Valerii Bihun <valeriibihun.co@gmail.com>
2022-05-18 02:06:28 -0600

    added background video

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,26 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="">
-      <div>
-        <h1>Cravings Defeated!</h1>
-      </div>
-      <div>
-        <h3> Let us suggest you a meal, my friend.</h3>
-      </div>
-      <div>
-      <Link href='/suggestion'>
-						<button className=''>
-							I`m ready for it!
-						</button>
-					</Link>
-      </div>
-    </main>
+    // <main className="flex h-screen justify-center items-center ">
+    //   <div className="bg-[#] text-center">
+    //     {/* <h1>Cravings Defeated!</h1>
+    //     <h3> Let us suggest you a meal, my friend.</h3> */}
+    //     <Link href="/suggestion">
+    //       <button className="">I`m ready for it!</button>
+    //     </Link>
+    //   </div>
+    // </main>
+
+    <section className="relative h-screen flex flex-col items-center justify-center text-center text-white py-0 px-3">
+    <div className="video-docker absolute top-0 left-0 w-full h-full overflow-hidden">
+        <video className="min-w-full min-h-full absolute object-cover" src="background/background.mp4" type="video/mp4" autoPlay muted loop></video>
+    </div>
+    <div className="video-content space-y-2">
+    <Link href="/suggestion">
+ <button className="">I`m ready for it!</button>
+    </Link>
+    </div>
+</section>
   );
 }
